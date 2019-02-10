3ef9e2ea808f45baeb556462030cc684f190d1cd
uzanka <uzanka>
2019-02-10 18:48:41 +0900

    bugfix

--- ver2/UnmovableFinder.cpp
+++ ver2/UnmovableFinder.cpp
@@ -10,24 +10,21 @@ UnmovableFinder::~UnmovableFinder() {
 
 ///////////////////////////////////////////////////////////////////////////////
 bool UnmovableFinder::Unmovable(const Table& table, const GoalsHolder& goals, const int x, const int y) {
-  if (table.Get(x, y) == kBox) {
-    if (Check1(table, goals, x, y)) {
-      return true;
-    }
-    if (Check2(table, goals, x, y)) {
-      return true;
-    }
-  }
+  if (table.Get(x, y) != kBox) return false;
+  if (goals.Goal(x, y)) return false;
+
+  if (Check11(table, x, y)) return true;
+  if (Check21(table, x, y)) return true;
+  if (Check22(table, x, y)) return true;
+  if (Check31(table, x, y)) return true;
+  if (Check41(table, x, y)) return true;
   return false;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-bool UnmovableFinder::Check1(const Table& table, const GoalsHolder& goals, const int x, const int y) {
+bool UnmovableFinder::Check11(const Table& table, const int x, const int y) {
   // #   $#   #  #$
   // $#  #   #$   #
-
-  if (table.Get(x, y) != kBox) return false;
-  if (goals.Goal(x, y)) return false;
 
   if ((table.Get(x + 1, y) == kWall) && (table.Get(x, y - 1) == kWall)) return true;
   if ((table.Get(x + 1, y) == kWall) && (table.Get(x, y + 1) == kWall)) return true;
@@ -38,33 +35,94 @@ bool UnmovableFinder::Check1(const Table& table, const GoalsHolder& goals, const
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-bool UnmovableFinder::Check2(const Table& table, const GoalsHolder& goals, const int x, const int y) {
+bool UnmovableFinder::Check21(const Table& table, const int x, const int y) {
   // C#  $C  #$  ##
   // $#  ##  #C  C$
 
-  if (table.Get(x, y) != kBox) return false;
-  if (goals.Goal(x, y)) return false;
-
   if ((table.Get(x + 1, y) == kWall) && (table.Get(x + 1, y + 1) == kWall)
-    && (table.Get(x, y + 1) == kBox)) return true;
-  if ((table.Get(x, y + 1) == kWall) && (table.Get(x + 1, y - 1) == kWall)
-    && (table.Get(x - 1, y) == kBox)) return true;
+   && (table.Get(x, y + 1) == kBox)) return true;
+  if ((table.Get(x, y + 1) == kWall) && (table.Get(x - 1, y + 1) == kWall)
+   && (table.Get(x - 1, y) == kBox)) return true;
   if ((table.Get(x - 1, y) == kWall) && (table.Get(x - 1, y - 1) == kWall)
-    && (table.Get(x, y - 1) == kBox)) return true;
+   && (table.Get(x, y - 1) == kBox)) return true;
   if ((table.Get(x, y - 1) == kWall) && (table.Get(x + 1, y - 1) == kWall)
-    && (table.Get(x + 1, y) == kBox)) return true;
+   && (table.Get(x + 1, y) == kBox)) return true;
 
   // $#  C$  #C  ##
   // C#  ##  #$  $C
 
   if ((table.Get(x + 1, y) == kWall) && (table.Get(x + 1, y - 1) == kWall)
-    && (table.Get(x, y - 1) == kBox)) return true;
+   && (table.Get(x, y - 1) == kBox)) return true;
   if ((table.Get(x, y + 1) == kWall) && (table.Get(x + 1, y + 1) == kWall)
-    && (table.Get(x + 1, y) == kBox)) return true;
+   && (table.Get(x + 1, y) == kBox)) return true;
   if ((table.Get(x - 1, y) == kWall) && (table.Get(x - 1, y + 1) == kWall)
-    && (table.Get(x, y + 1) == kBox)) return true;
+   && (table.Get(x, y + 1) == kBox)) return true;
   if ((table.Get(x, y - 1) == kWall) && (table.Get(x - 1, y - 1) == kWall)
-    && (table.Get(x - 1, y) == kBox)) return true;
+   && (table.Get(x - 1, y) == kBox)) return true;
+
+  return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+bool UnmovableFinder::Check22(const Table& table, const int x, const int y) {
+  // ?C#  #?  ?$#  #?
+  // #$?  $C  #C?  C$
+  //      ?#       ?#
+
+  if ((table.Get(x + 1, y) == kWall) && (table.Get(x - 1, y + 1) == kWall)
+   && (table.Get(x, y + 1) == kBox)) return true;
+  if ((table.Get(x - 1, y - 1) == kWall) && (table.Get(x, y + 1) == kWall)
+   && (table.Get(x - 1, y) == kBox)) return true;
+  if ((table.Get(x + 1, y - 1) == kWall) && (table.Get(x - 1, y) == kWall)
+   && (table.Get(x, y - 1) == kBox)) return true;
+  if ((table.Get(x, y - 1) == kWall) && (table.Get(x + 1, y + 1) == kWall)
+   && (table.Get(x + 1, y) == kBox)) return true;
+
+  return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+bool UnmovableFinder::Check31(const Table& table, const int x, const int y) {
+  // C#  $C  $$  #$
+  // $$  $#  #C  C$
+
+  if ((table.Get(x + 1, y) == kWall)
+   && (table.Get(x, y + 1) == kBox) && (table.Get(x + 1, y + 1) == kBox)) return true;
+  if ((table.Get(x, y + 1) == kWall)
+   && (table.Get(x - 1, y) == kBox) && (table.Get(x - 1, y + 1) == kBox)) return true;
+  if ((table.Get(x - 1, y) == kWall)
+   && (table.Get(x - 1, y - 1) == kBox) && (table.Get(x, y - 1) == kBox)) return true;
+  if ((table.Get(x, y - 1) == kWall)
+   && (table.Get(x + 1, y - 1) == kBox) && (table.Get(x + 1, y) == kBox)) return true;
+
+  // #C  $#  $$  C$
+  // $$  $C  C#  #$
+
+  if ((table.Get(x - 1, y) == kWall)
+   && (table.Get(x - 1, y + 1) == kBox) && (table.Get(x, y + 1) == kBox)) return true;
+  if ((table.Get(x, y - 1) == kWall)
+   && (table.Get(x - 1, y - 1) == kBox) && (table.Get(x - 1, y) == kBox)) return true;
+  if ((table.Get(x + 1, y) == kWall)
+   && (table.Get(x, y - 1) == kBox) && (table.Get(x + 1, y - 1) == kBox)) return true;
+  if ((table.Get(x, y + 1) == kWall)
+   && (table.Get(x + 1, y) == kBox) && (table.Get(x + 1, y + 1) == kBox)) return true;
+
+  return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+bool UnmovableFinder::Check41(const Table& table, const int x, const int y) {
+  // C$  $C  $$  $$
+  // $$  $$  $C  C$
+
+  if ((table.Get(x + 1, y) == kBox)
+   && (table.Get(x, y + 1) == kBox) && (table.Get(x + 1, y + 1) == kBox)) return true;
+  if ((table.Get(x, y + 1) == kBox)
+   && (table.Get(x - 1, y) == kBox) && (table.Get(x - 1, y + 1) == kBox)) return true;
+  if ((table.Get(x - 1, y) == kBox)
+   && (table.Get(x - 1, y - 1) == kBox) && (table.Get(x, y - 1) == kBox)) return true;
+  if ((table.Get(x, y - 1) == kBox)
+   && (table.Get(x + 1, y - 1) == kBox) && (table.Get(x + 1, y) == kBox)) return true;
 
   return false;
 }
